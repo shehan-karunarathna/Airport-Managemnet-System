7a933e80b9bc6fad211f1078b70dbf28ec2fffa0
Shehan - karunarathne <shehankarunarathna528@gmail.com>
2024-08-17 10:56:24 +0530

    new change

--- src/views/AirPlane/AirplaneReg.js
+++ src/views/AirPlane/AirplaneReg.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom' // Import useNavigate
+import { useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
